d53f88f20a505bdea5c36714fc2d9e414be18688
Akhil <56110114+AKHILKRISHNA-CPU@users.noreply.github.com>
2022-12-30 14:22:35 +0530

    Update Question2_SecondQuery.sql

--- SQL Question 2/Question2_SecondQuery.sql
+++ SQL Question 2/Question2_SecondQuery.sql
@@ -1,7 +1,7 @@
 -- Create a table name - “Football Venue”
 
 
-CREATE TABLE Football Venue (
+CREATE TABLE FootballVenue (
     Venue_id INTEGER PRIMARY KEY NOT NULL,
     Venue_name VARCHAR(255) NOT NULL,
     City_id INTEGER NOT NULL,
